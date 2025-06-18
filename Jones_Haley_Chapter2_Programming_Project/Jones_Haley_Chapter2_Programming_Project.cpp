889a122a1f2fa6caf80ef75d6b404a4fb8d4fcb8
haj2455 <haleybear057@gmail.com>
2025-06-18 10:40:16 -0400

    code before testing

--- Jones_Haley_Chapter2_Programming_Project/Jones_Haley_Chapter2_Programming_Project.cpp
+++ Jones_Haley_Chapter2_Programming_Project/Jones_Haley_Chapter2_Programming_Project.cpp
@@ -6,5 +6,17 @@ using namespace std;
 
 int main()
 {
+	double millimeters = 1.5;
+	double year1 = 5, year2 = 7, year3 = 10;
+	double oceanlevel1, oceanlevel2, oceanlevel3;
 
+	oceanlevel1 = millimeters * year1;
+	oceanlevel2 = millimeters * year2;
+	oceanlevel3 = millimeters * year3;
+
+	cout << "In " << year1 << "the ocean level will be " << oceanlevel1 << "millimeters" << endl;
+	cout << "In " << year2 << "the ocean level will be " << oceanlevel2 << "millimeters" << endl;
+	cout << "In " << year3 << "the ocean level will be " << oceanlevel3 << "millimeters" << endl;
+
+	return 0;
 }
