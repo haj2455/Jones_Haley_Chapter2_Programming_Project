4d76b17f93209a11add48fda25168f94396fffa0
haj2455 <haleybear057@gmail.com>
2025-06-18 10:16:24 -0400

    save changes

--- Jones_Haley_Chapter2_Programming_Project/Jones_Haley_Chapter2_Programming_Project.cpp
+++ Jones_Haley_Chapter2_Programming_Project/Jones_Haley_Chapter2_Programming_Project.cpp
@@ -1,6 +1,10 @@
 // Jones_Haley_Chapter2_Programming_Project.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+//Ocean Levels Rising
 
 #include <iostream>
+using namespace std;
 
 int main()
+{
+
+}
