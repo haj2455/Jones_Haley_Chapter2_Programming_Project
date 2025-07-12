60cbecaf60dbeb1af448bbc2122d7734a837847a
haj2455 <haleybear057@gmail.com>
2025-07-12 16:08:39 -0400

    adding title

--- Jones_Haley_Chapter2_Programming_Project/Jones_Haley_Chapter2_Programming_Project.cpp
+++ Jones_Haley_Chapter2_Programming_Project/Jones_Haley_Chapter2_Programming_Project.cpp
@@ -1,5 +1,5 @@
 // Jones_Haley_Chapter2_Programming_Project.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+//Traveling on a tank of gas
 
 #include <iostream>
 
