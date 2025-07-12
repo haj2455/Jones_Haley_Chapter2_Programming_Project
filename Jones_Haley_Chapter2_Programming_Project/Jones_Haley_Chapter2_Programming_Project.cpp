7cf50a69d81fa719272c5cdd8c03076ac246ba28
haj2455 <haleybear057@gmail.com>
2025-07-12 16:18:30 -0400

    Finished code

--- Jones_Haley_Chapter2_Programming_Project/Jones_Haley_Chapter2_Programming_Project.cpp
+++ Jones_Haley_Chapter2_Programming_Project/Jones_Haley_Chapter2_Programming_Project.cpp
@@ -6,4 +6,16 @@ using namespace std;
 
 int main() {
 
+	const double tank_capacity = 20.0;
+	const double mpg_town = 23.5;
+	const double mpg_highway = 28.9;
+
+	double distancetown = tank_capacity * mpg_town;
+	double distancehighway = tank_capacity * mpg_highway;
+
+	cout << "The amount of distance the car can travel in town on one tank of gas is " << distancetown << " miles." << endl;
+	cout << "The amount of distnce the car can travel on the highway on one tank of gas is " << distancehighway << " miles." << endl;
+
+	return 0;
+
 }
